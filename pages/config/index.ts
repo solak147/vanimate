ecba551aa2effd933b03d0e1d0d7b41c1af9289c
solak1 <rohanasdf12@gmail.com>
2024-06-05 00:03:08 +0800

    fix marquee

---
+++ pages/config/index.ts
@@ -0,0 +1,80 @@
+export const animateCfg = [
+  [
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/53/df229f51a820b7a21c2a2f63591q6md5.JPG',
+      title: '隊長小翼 Season2 青少年篇'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/61/b20651176cc7b3451be922675d1q6s55.JPG',
+      title: '鬼滅之刃 柱訓練篇'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/89/4bbbf6144ca2ec7f41d5a218ae1q79l5.JPG',
+      title: '蔚藍檔案 The Animation'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/94/2bac2fe57904effeb6792cd9841q7ci5.JPG',
+      title: '無職轉生～到了異世界就拿出真本事 第二季'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/95/df4292e2870356ce102261f7591q7cj5.JPG',
+      title: '老夫老妻重返青春'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/03/da9f53a5c2d30d4d53c98ee07d1q7775.JPG',
+      title: '轉生貴族憑鑑定技能扭轉人生'
+    }
+  ],
+  [
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/54/26a6decd1c0a7c2a33900784801q6me5.JPG',
+      title: '死神少爺與黑女僕 第三季'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/73/d33c1038bad72d42a9b5da56481q7bx5.JPG',
+      title: '恰如細語般的戀歌'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/60/f4e8a3bae91f2452639378b13c1q7pg5.JPG',
+      title: '轉生為第七王子，隨心所欲的魔法學習之路'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/72/ac7fa56096c5129fa35f54e9bc1q7bw5.JPG',
+      title: 'THE NEW GATE'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/71/ea1f6c749c39278e78195623331q7bv5.JPG',
+      title: '夜晚的水母不會游泳'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/70/8b85c3f311b5442d7a219833d21q7bu5.JPG',
+      title: '黑執事 寄宿學校篇'
+    }
+  ],
+  [
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/32/f497571a0ae490a860daa22cef1q6zo5.JPG',
+      title: '怪獸 8 號'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/69/a233c711c67a102a21a17b4ffc1q7bt5.JPG',
+      title: '龍族'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/79/33bad5b28f53ed0eaa19f02a931q6vf5.JPG',
+      title: '單人房、日照一般、附天使。'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/91/642cc2ec84dc9339e0c2e19d131q71b5.JPG',
+      title: '我的英雄學院 第七季'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/29/a65fdac15052166ecb537078b91q6u15.JPG',
+      title: '蜻蛉高球'
+    },
+    {
+      img: 'https://p2.bahamut.com.tw/B/2KU/24/8d404b2cb13b22f67b64e4c05a1q6r45.JPG',
+      title: '魔法科高中的劣等生 第三季'
+    }
+  ]
+]
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <v-tabs v-model="tab" hide-slider selected-class="activeTab" class="mt-10">
-      <v-tab value="one">本季新翻</v-tab>
-      <v-tab value="two">週期表</v-tab>
-    </v-tabs>
+    <div class="d-flex justify-space-between mt-10 ml-5 mr-5">
+      <v-tabs v-model="tab" hide-slider selected-class="activeTab">
+        <v-tab value="one">本季新翻</v-tab>
+        <v-tab value="two">週期表</v-tab>
+      </v-tabs>
+
+      <v-menu>
+        <template #activator="{ props }">
+          <v-btn v-bind="props" append-icon="mdi-filter-variant" variant="text">
+            依更新日排列
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in order" :key="index" :value="index">
+            <v-list-item-title>
+              <v-icon icon="mdi-check" color="primary" class="mr-1"></v-icon
+              >{{ item.title }}</v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
 
     <v-window v-model="tab">
       <v-window-item value="one">
@@ -52,102 +70,34 @@
                   class="d-flex"
                   :class="{ animateTxt: i.animateTxt }"
                 >
-                  <div>{{ i.title }}</div>
+                  <div style="width: 200px">{{ i.title }}</div>
 
-                  <div class="pl-16">{{ i.title }}</div>
+                  <div>{{ i.title }}</div>
                 </div>
               </div>
               <div><v-icon icon="mdi-eye" class="mr-1"></v-icon>89萬</div>
             </div>
           </v-col>
         </v-row>
+
+        <div class="d-flex justify-center">
+          <v-btn class="mt-5 text-h5" color="primary" variant="elevated" width="300" height="60"
+            >展開更多</v-btn
+          >
+        </div>
+
+        <h1 class="mt-10 ml-5">近期熱播</h1>
       </v-window-item>
     </v-window>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { animateCfg } from './config'
+
 const tab = ref(null)
-const animateImg = ref([
-  [
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/53/df229f51a820b7a21c2a2f63591q6md5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/61/b20651176cc7b3451be922675d1q6s55.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/89/4bbbf6144ca2ec7f41d5a218ae1q79l5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/94/2bac2fe57904effeb6792cd9841q7ci5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/95/df4292e2870356ce102261f7591q7cj5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/03/da9f53a5c2d30d4d53c98ee07d1q7775.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    }
-  ],
-  [
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/54/26a6decd1c0a7c2a33900784801q6me5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/73/d33c1038bad72d42a9b5da56481q7bx5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/03/da9f53a5c2d30d4d53c98ee07d1q7775.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/72/ac7fa56096c5129fa35f54e9bc1q7bw5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/71/ea1f6c749c39278e78195623331q7bv5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/70/8b85c3f311b5442d7a219833d21q7bu5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    }
-  ],
-  [
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/32/f497571a0ae490a860daa22cef1q6zo5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/69/a233c711c67a102a21a17b4ffc1q7bt5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/79/33bad5b28f53ed0eaa19f02a931q6vf5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/91/642cc2ec84dc9339e0c2e19d131q71b5.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/29/a65fdac15052166ecb537078b91q6u15.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    },
-    {
-      img: 'https://p2.bahamut.com.tw/B/2KU/24/8d404b2cb13b22f67b64e4c05a1q6r45.JPG',
-      title: '隊長小翼 Season2 青少年篇'
-    }
-  ]
-])
+const animateImg = toRef(animateCfg)
+const order = ref([{ title: '依更新日排列' }, { title: '依平均人氣排列' }])
 
 const seasons = ref({
   0: 'Winter',
@@ -187,13 +137,14 @@ const test = (val: Array<number>) => {
 }
 
 .animateTxt {
-  animation: marquee 3s linear infinite normal forwards;
+  animation: 4s marquee linear infinite;
 }
 
 @keyframes marquee {
   0% {
-    transform: translateX(0);
+    transform: translateX(60%);
   }
+
   100% {
     transform: translateX(-100%);
   }
